9c3f0673a3d892444845ad2b9f89bf9683dbf7df
Imran Farooq <imran_farooq7@hotmail.com>
2023-12-27 23:18:56 +0500

    Dispatch actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import { useSelector } from "react-redux";
 import AccountOperation from "./components/AccountOperation";
 import CreateCustomer from "./components/CreateCustomer";
 import Customer from "./components/Customer";
 
 const App = () => {
+	const fullName = useSelector((state) => state.customer.fullName);
 	return (
 		<div>
 			<h1>🏦 The React-Redux Bank ⚛️</h1>
-			<CreateCustomer />
-			<Customer />
-			<AccountOperation />
+			{fullName === "" ? (
+				<CreateCustomer />
+			) : (
+				<>
+					<Customer />
+					<AccountOperation />
+				</>
+			)}
 		</div>
 	);
 };
--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 
 const Customer = () => {
 	const customerState = useSelector((state) => state.customer);
-	console.log(customerState, "customomerState");
+	// console.log(customerState, "customomerState");
 	return (
 		<div>
 			<h2>👋 Welcome, {customerState.fullName}</h2>
